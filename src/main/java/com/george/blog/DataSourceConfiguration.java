3f86ccdb9217854a13b6be64299d831aa7119923
George Sheng <gsheng112@gmail.com>
2021-07-20 16:07:10 -0400

    b DataSourceConfiguration.java

--- src/main/java/com/george/blog/DataSourceConfiguration.java
+++ src/main/java/com/george/blog/DataSourceConfiguration.java
@@ -1,5 +1,8 @@
 package com.george.blog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +11,34 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfiguration.class);
+    private String driver;
+    private String url;
+    private String userId;
+    private String password;
+
+    public DataSourceConfiguration (
+        @Value("${driver}") String driver,
+        @Value("${url}") String url,
+        @Value("${userId}") String userId,
+        @Value("${password}") String password
+    ) {
+        this.driver = driver;
+        this.url = url;
+        this.userId = userId;
+        this.password = password;
+    }
+
     @Bean
     public DataSource datasource() {
-        return DataSourceBuilder.create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/knowledge_base")
-                .username("root")
-                .password("new_password")
+        DataSource ds =  DataSourceBuilder.create()
+                .driverClassName(this.driver)
+                .url(this.url)
+                .username(this.userId)
+                .password(this.password)
                 .build();
+        logger.info("data source created =====> {}@{}", this.userId, this.url);
+        return ds;
     }
 
 }
