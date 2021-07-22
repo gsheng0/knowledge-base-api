8ddf37cd4181ddfe2a82fadd01860af30ae899b4
George Sheng <gsheng112@gmail.com>
2021-07-21 21:24:02 -0400

    b add logging

--- src/main/java/com/george/blog/Controller.java
+++ src/main/java/com/george/blog/Controller.java
@@ -3,11 +3,14 @@ package com.george.blog;
 import com.george.blog.components.Article;
 import com.george.blog.components.DBHandler;
 import com.george.blog.components.UploadedArticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,6 +18,8 @@ import java.util.Date;
 @CrossOrigin("*")
 @RestController
 public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @Autowired
     private JdbcTemplate template;
     private int counter = 0;
@@ -26,27 +31,32 @@ public class Controller {
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home(HttpServletRequest request) {
+        logger.info("Request Head: {}", request);
         return "Blog Page Home";
     }
 
     @PostMapping("/search")
-    public ArrayList<Article> search(@RequestBody String query){
+    public ArrayList<Article> search(HttpServletRequest request, @RequestBody String query){
+        logger.info("{} search: {}", request, query);
         return (ArrayList<Article>)DBHandler.searchByTerm(query);
     }
 
     @PostMapping("/get")
     public Article select(@RequestBody int id){
+        logger.info("get: {}", id);
         return DBHandler.getArticleById(id);
     }
 
     @GetMapping("/view-all")
     public ArrayList<Article> viewAll() {
+        logger.info("view-all");
         return (ArrayList<Article>)DBHandler.getAllArticles();
     }
 
     @PostMapping("/upload")
     public boolean uploadArticle(@RequestBody UploadedArticle uploadedArticle){
+        logger.info("upload: {]", uploadedArticle.getTitle());
         Article article = new Article(counter, uploadedArticle.getTitle(), uploadedArticle.getContent(), new Date().toString());
         int response = DBHandler.insertArticle(article);
         if(response > 0)
